279cc6e320bd47669ebdb49d0edec17d8fd04974
eli222eli <a0533151818@gmail.com>
2025-10-20 18:21:15 +0300

    Update main_dowalding.py

--- main_dowalding.py
+++ main_dowalding.py
@@ -7,10 +7,10 @@ import tempfile
 import shutil
 
 # הקישור לאתר שממנו הגענו (Referer)
-WEBSITE_URL = 'https://www.apkbe.com/down/com.ldw.fishtycoon2_1.7.6_free'
+WEBSITE_URL = 'https://apkcombo.com/temo-3/com.techem.temodokuapp/download/apk'
 
 # הקישור לקובץ ההורדה
-DOWNLOAD_URL = 'https://dl.apkbe.com/down.do/com.ldw.fishtycoon2_1.7.6_free?code=46c7299bae4dc07c9a886c6a0104c342'
+DOWNLOAD_URL = 'https://apkcombo.com/d?u=aHR0cHM6Ly9kb3dubG9hZC5wdXJlYXBrLmNvbS9iL1hBUEsvWTI5dExuUmxZMmhsYlM1MFpXMXZaRzlyZFdGd2NGOHhNalV5TVRNM05EZ3hYMkZrT0dFNE5HSmk/YXMyPWRjYWJiYzQ5NWM0NmE5ODUzNTM5OWRjMGE0ZjIwZTIzNmFkMGVlYzkmaz1hNzViZDYwMDJiOTM4ZDAxMmQ5ZjA1ZDZiMjcyZGUxODZhZDBlZWM5Jl9wPVkyOXRMblJsWTJobGJTNTBaVzF2Wkc5cmRXRndjQSZjPTElN0NCVVNJTkVTUyU3Q2IybGtQVGttWkdWMlBWUmxZMmhsYlNVeU1FVnVaWEpuZVNVeU1GTmxjblpwWTJWekpUSXdKVEl3UjIxaVNDWjBQWGhoY0dzbWN6MDJNRFU0T0RBeU9TWjJiajB4TGpJMUxqSXhMak0zTkRneExXMWhjM1JsY2laMll6MHhNalV5TVRNM05EZ3gmX2ZuPVZHVk5ieXN6WHpFdU1qVXVNakV1TXpjME9ERXRiV0Z6ZEdWeVgyRndhMk52YldKdkxtTnZiUzU0WVhCcg==&fp=634eb4a48bbcfd76f2de506f2eb496d7&ip=31.154.27.134&package_name=com.techem.temodokuapp&lang=en'
 
 
 # הורדת קבצים מהאנטרנט, יש למלא את הקישור של האתר וגם את הרישור של ההורדה
